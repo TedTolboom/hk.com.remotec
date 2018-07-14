32b8cd3284fdf72e19a69818db5995ce8163fb56
dT <dTNL.Homey@gmail.com>
2018-07-14 13:27:00 +0200

    Driver update and cleanup

--- drivers/ZXT-120/device.js
+++ drivers/ZXT-120/device.js
@@ -13,29 +13,21 @@ class ZXT120 extends ZwaveDevice {
 		this.printNode();
 
 		new Homey.FlowCardAction('setmode')
-					.register()
-					.registerRunListener(this._onModeChange.bind(this));
+			.register()
+			.registerRunListener(this._onModeChange.bind(this));
 
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
 			getOpts: {
 				getOnStart: true, // get the initial value on app start
-			}
-		});
-		this.registerCapability('alarm_battery', 'BATTERY', {
-			report: 'BATTERY_REPORT',
-			getOpts: {
-				//getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
 			},
-			reportParser(report) {
-				return report['Battery Level'] === 'battery low warning';
-			}
 		});
+		this.registerCapability('measure_battery', 'BATTERY');
+		this.registerCapability('alarm_battery', 'BATTERY');
 
 		this.registerCapability('AC_mode', 'THERMOSTAT_MODE', {
 			getOpts: {
-				//getOnOnline: true,
-				//getOnStart: true, // get the initial value on app start
+				// getOnOnline: true,
+				getOnStart: true, // get the initial value on app start
 				//pollInterval: 'poll_interval_MODE' // maps to device settings
 			},
 			get: 'THERMOSTAT_MODE_GET',
@@ -67,17 +59,14 @@ class ZXT120 extends ZwaveDevice {
 				getOnStart: true,
 			},
 			set: 'THERMOSTAT_SETPOINT_SET',
-			setParserV2: value =>{
+			setParserV2: value => {
 				// Create value buffer
-				const bufferValue = new Buffer(2);
-				bufferValue.writeUInt16BE((Math.round(value * 2) / 2 * 10).toFixed(0));
+				const setPointValue = new Buffer(2);
+				setPointValue.writeUInt16BE((Math.round(value * 2) / 2 * 10).toFixed(0));
 				let setPointType = 'Heating 1';
-				let UIMode = this.getCapabilityValue('AC_mode');
-				if (UIMode === "Cool")
-					setPointType = 'Cooling 1';
-
-				return{
-
+				const UIMode = this.getCapabilityValue('AC_mode');
+				if (UIMode === 'Cool') setPointType = 'Cooling 1';
+				return {
 					Level: {
 						'Setpoint Type': setPointType,
 					},
@@ -86,8 +75,8 @@ class ZXT120 extends ZwaveDevice {
 						Scale: 0,
 						Precision: 1,
 					},
-					Value: bufferValue,
-				}
+					Value: setPointValue,
+				};
 			},
 			report: 'THERMOSTAT_SETPOINT_REPORT',
 			reportParserV2: report => {
@@ -117,34 +106,34 @@ class ZXT120 extends ZwaveDevice {
 		});
 
 
-		// this.registerCapability('FAN_mode', 'THERMOSTAT_FAN_MODE', {
-		// 	getOpts: {
-		// 		//getOnOnline: true,
-		// 		// getOnStart: true, // get the initial value on app start
-		// 		//pollInterval: 'poll_interval' // maps to device settings
-		// 	},
-		// 	get: 'THERMOSTAT_FAN_MODE_GET',
-		// 	set: 'THERMOSTAT_FAN_MODE_SET',
-		// 	setParserV2: value => {
-		// 		this.log('Setting FAN mode to:', value);
-		// 		return {
-		// 			Properties1: {
-		// 				'Fan Mode': value,
-		// 				//'Reserved':
-		// 				Off: false,
-		// 			},
-		// 		};
-		// 	},
-		// 	report: 'THERMOSTAT_FAN_MODE_REPORT',
-		// 	reportParserV2: report => {
-		// 		if (!report) return null;
-		// 		this.log('FAN Mode Report:', report);
-		// 		if (report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Mode')) {
-		// 			return report.Level.Mode;
-		// 		}
-		// 		return null;
-		// 	}
-		// });
+		this.registerCapability('FAN_mode', 'THERMOSTAT_FAN_MODE', {
+			getOpts: {
+				// getOnOnline: true,
+				getOnStart: true, // get the initial value on app start
+				// 		//pollInterval: 'poll_interval' // maps to device settings
+			},
+			get: 'THERMOSTAT_FAN_MODE_GET',
+			set: 'THERMOSTAT_FAN_MODE_SET',
+			setParserV2: value => {
+				this.log('Setting FAN mode to:', value);
+				return {
+					Level: {
+						'Fan Mode': value,
+						// 'Reserved':
+						Off: false,
+					},
+				};
+			},
+			report: 'THERMOSTAT_FAN_MODE_REPORT',
+			reportParserV2: report => {
+				if (!report) return null;
+				this.log('FAN Mode Report:', report);
+				if (report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Mode')) {
+					return report.Level.Mode;
+				}
+				return null;
+			},
+		});
 
 		// register a report listener
 		this.registerReportListener('BATTERY', 'BATTERY_REPORT', (rawReport, parsedReport) => {
@@ -157,7 +146,7 @@ class ZXT120 extends ZwaveDevice {
 
 	}
 
-	setMode( data ) {
+	setMode(data) {
 		console.log(data);
 		this.triggerCapabilityListener('AC_mode', data);
 		return true;
