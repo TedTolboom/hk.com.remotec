238f0d5c84ffefd836ad4d1a0f3f7661174a06c4
dT <dTNL.Homey@gmail.com>
2018-07-15 11:45:59 +0200

    Report parsing update

--- drivers/ZXT-120/device.js
+++ drivers/ZXT-120/device.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 const Homey = require('homey');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+// Documentation:
 
 class ZXT120 extends ZwaveDevice {
 	onMeshInit() {
@@ -16,21 +18,27 @@ class ZXT120 extends ZwaveDevice {
 			.register()
 			.registerRunListener(this._onModeChange.bind(this));
 
+		new Homey.FlowCardAction('setfanspeed')
+			.register()
+			.registerRunListener(this._onFanSpeedChange.bind(this));
+
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
 			getOpts: {
+				getOnOnline: true,
 				getOnStart: true, // get the initial value on app start
 			},
 		});
+
 		this.registerCapability('measure_battery', 'BATTERY');
 		this.registerCapability('alarm_battery', 'BATTERY');
 
 		this.registerCapability('AC_mode', 'THERMOSTAT_MODE', {
 			getOpts: {
-				// getOnOnline: true,
+				getOnOnline: true,
 				getOnStart: true, // get the initial value on app start
 				//pollInterval: 'poll_interval_MODE' // maps to device settings
 			},
-			get: 'THERMOSTAT_MODE_GET',
+			get: 'THERMOSTAT_MODE_GET', // RAW NodeID, 0x40,0x02
 			set: 'THERMOSTAT_MODE_SET',
 			setParserV2: value => {
 				console.log('Setting mode to:', value);
@@ -43,20 +51,30 @@ class ZXT120 extends ZwaveDevice {
 				};
 			},
 			report: 'THERMOSTAT_MODE_REPORT',
-			reportParserV3: report => {
+			reportParserV2: report => {
 				if (!report) return null;
-				console.log('Mode Report:', report);
 				if (report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Mode')) {
+					console.log('Mode Report received:', report.Level.Mode);
 					return report.Level.Mode;
 				}
 				return null;
-			}
+			},
 		});
 
 		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT', {
-			get: 'THERMOSTAT_SETPOINT_GET',
+			get: 'THERMOSTAT_SETPOINT_GET', // RAW NodeID, 0x43,0x02, {0x01 Heating 1 / 0x02 Cooling 1}
 			getOpts: {
 				getOnStart: true,
+			},
+			getParser: () => {
+				let setPointType = 'Heating 1';
+				const UIMode = this.getCapabilityValue('AC_mode');
+				if (UIMode === 'Cool') setPointType = 'Cooling 1';
+				return {
+					Level: {
+						'Setpoint Type': setPointType,
+					},
+				};
 			},
 			set: 'THERMOSTAT_SETPOINT_SET',
 			setParserV2: value => {
@@ -81,8 +99,10 @@ class ZXT120 extends ZwaveDevice {
 			report: 'THERMOSTAT_SETPOINT_REPORT',
 			reportParserV2: report => {
 				if (!report) return null;
-				this.log('Setpoint Report:', report);
-				if (report && report.hasOwnProperty('Level2') &&
+				// this.log('Setpoint Report:', report);
+				if (report && report.hasOwnProperty('Level') &&
+					report.Level.hasOwnProperty('Setpoint Type') &&
+					report.hasOwnProperty('Level2') &&
 					report.Level2.hasOwnProperty('Scale') &&
 					report.Level2.hasOwnProperty('Precision') &&
 					report.Level2.Scale === 0 &&
@@ -97,14 +117,16 @@ class ZXT120 extends ZwaveDevice {
 					}
 
 					if (typeof readValue !== 'undefined') {
-						return readValue / Math.pow(10, report.Level2.Precision);
+						const setPointValue = readValue / Math.pow(10, report.Level2.Precision);
+						const setPointType = report.Level['Setpoint Type'];
+						this.log('Setpoint Report received: Setpoint type', setPointType, ' Setpoint value', setPointValue);
+						return setPointValue;
 					}
 					return null;
 				}
 				return null;
-			}
+			},
 		});
-
 
 		this.registerCapability('FAN_mode', 'THERMOSTAT_FAN_MODE', {
 			getOpts: {
@@ -112,7 +134,7 @@ class ZXT120 extends ZwaveDevice {
 				getOnStart: true, // get the initial value on app start
 				// 		//pollInterval: 'poll_interval' // maps to device settings
 			},
-			get: 'THERMOSTAT_FAN_MODE_GET',
+			get: 'THERMOSTAT_FAN_MODE_GET', // RAW NodeID, 0x44,0x02
 			set: 'THERMOSTAT_FAN_MODE_SET',
 			setParserV2: value => {
 				this.log('Setting FAN mode to:', value);
@@ -127,9 +149,9 @@ class ZXT120 extends ZwaveDevice {
 			report: 'THERMOSTAT_FAN_MODE_REPORT',
 			reportParserV2: report => {
 				if (!report) return null;
-				this.log('FAN Mode Report:', report);
-				if (report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Mode')) {
-					return report.Level.Mode;
+				if (report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Fan Mode')) {
+					this.log('FAN Mode Report received:', report.Level['Fan Mode']);
+					return report.Level['Fan Mode'];
 				}
 				return null;
 			},
@@ -152,8 +174,18 @@ class ZXT120 extends ZwaveDevice {
 		return true;
 	}
 
+	setFanSpeed(data) {
+		console.log(data);
+		this.triggerCapabilityListener('FAN_mode', data);
+		return true;
+	}
+
 	_onModeChange(args) {
 		return args.device.setMode(args.mode);
+	}
+
+	_onFanSpeedChange(args) {
+		return args.device.setFanSpeed(args.fanspeed);
 	}
 
 }
